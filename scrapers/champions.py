3318bf90700b7140823451334b5807b95049591d
John <john4button@gmail.com>
2024-05-18 16:27:52 -0400

    full champion data

--- scrapers/champions.py
+++ scrapers/champions.py
@@ -5,69 +5,40 @@ from bs4 import BeautifulSoup
 
 data = []
 
-for season in range(11, 15):
-    for split in range(1, 3):
+def get_champion_data(season, split):
+    # URL of the page to scrape
+    url = f"https://gol.gg/champion/list/season-S{season}/split-{split}/tournament-ALL/"
 
-        if season == 14 and split == 2:
-            continue
+    # Headers to mimic a browser visit
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
 
-        split_name = "None"
+    # Sending a GET request to the URL with headers
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # Ensures we notice bad responses
 
-        if split == 1:
-            split_name = "Spring"
-        elif split == 2:
-            split_name = "Summer"
+    # Parsing the HTML content
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-        # URL of the page to scrape
-        url = "https://gol.gg/champion/list/season-S" + str(season) + "/split-" + str(split_name) + "/tournament-ALL/"
+    # Finding the table containing the data
+    champions_table = soup.find('table', class_='table_list')
 
-        # Headers to mimic a browser visit
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-        }
-
-        # Sending a GET request to the URL with headers
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Ensures we notice bad responses
-
-        # Parsing the HTML content
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Finding the table containing the data
-        champions_table = soup.find('table', class_='table_list')
-
-        # Parsing each row in the table
-        for row in champions_table.find_all('tr')[1:]:  # skipping the header row
-            cols = row.find_all('td')
-            if cols:
-                champion_name = cols[0].text.strip()
-                picks = cols[1].text.strip()
-                bans = cols[2].text.strip()
-                presence = cols[3].text.strip()
-                wins = cols[4].text.strip()
-                losses = cols[5].text.strip()
-                win_rate = cols[6].text.strip()
-                kda = cols[7].text.strip()
-                avg_bt = cols[8].text.strip()
-                avg_gt = cols[9].text.strip()
-                csm = cols[10].text.strip()
-                dpm = cols[11].text.strip()
-                gpm = cols[12].text.strip()
-                data.append({
-                    'Champion': champion_name + " S" + str(season) + " " + str(split_name),
-                    'Picks': picks,
-                    'Bans': bans,
-                    'Presence': presence,
-                    'Wins': wins,
-                    'Losses': losses,
-                    'Win Rate': win_rate,
-                    'KDA': kda,
-                    'Avg BT': avg_bt,
-                    'Avg GT': avg_gt,
-                    'CSM': csm,
-                    'DPM': dpm,
-                    'GPM': gpm
-                })
+    rows = champions_table.find_all('tr')
+    data = []
+    
+    for row in rows[1:]:
+        cols = row.find_all('td')
+        link = cols[0].find('a')
+        row_data = [f"S{season}", link.get_text().strip(), link['href'].split('/')[2]]
+        for col in cols[1:]:
+            text = col.get_text().strip()
+            if text:
+                row_data.append(col.get_text().strip())
+            else: row_data.append('-')
+        data.append(row_data)
+        
+    return data
 
 
 # Defining the CSV file path
@@ -75,8 +46,12 @@ csv_file_path = '../data/champions.csv'
 
 # Writing data to CSV
 with open(csv_file_path, mode='w', newline='') as file:
-    writer = csv.DictWriter(file,
-                            fieldnames=['Champion', 'Picks', 'Bans', 'Presence', 'Wins', 'Losses', 'Win Rate', 'KDA',
-                                        'Avg BT', 'Avg GT', 'CSM', 'DPM', 'GPM'])
-    writer.writeheader()
-    writer.writerows(data)
+    writer = csv.writer(file)
+    writer.writerow(['Season', 'Champion', 'Picks', 'Bans', 'Presence', 'Wins', 'Losses', 'Win Rate', 'KDA',
+                                        'Avg BT', 'Avg GT', 'CSM', 'DPM', 'GPM', 'CSD@15', 'GD@15', 'XPD@15'])
+    for season in range(11, 15):
+        for split in ["Spring", "Summer"]:
+            for champ in get_champion_data(season, split):
+                writer.writerow(champ)
+    
+    print("Data written to champions.csv")
