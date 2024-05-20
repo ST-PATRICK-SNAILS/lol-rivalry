e39acce8e378f18b5c0d81aa3d1fdd677accec25
John <john4button@gmail.com>
2024-05-20 11:16:15 -0400

    fix player propagation bug

--- pipeline/playerPropagation.py
+++ pipeline/playerPropagation.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 def playerPropagate():
     teamdata_dfs = []
-    for index, row in indexer.records['Event'].iterrows():
+    for _, row in indexer.records.iterrows():
         season, split, team1_id, team2_id = row["Season"], row["Split"], row["Team 1 ID"], row["Team 2 ID"]
         roster_data = indexer.getRosterDataByTeams(season, split, team1_id, team2_id)
         teamdata_dfs.append(roster_data)
         
     combined_players_df = pd.concat(teamdata_dfs, ignore_index=True)
     return combined_players_df
+
+print(playerPropagate())
